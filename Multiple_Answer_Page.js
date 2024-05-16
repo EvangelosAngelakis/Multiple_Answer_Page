d0814e0e109b8afbc965b0f079b56448b3ef300b
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-05-16 15:09:17 +0100

    First Commit of HTML, CSS and JS

---
+++ Multiple_Answer_Page.js
@@ -0,0 +1,12 @@
+function changeMessage1() {
+    document.getElementById("q1").innerHTML = "Great Answer";
+}
+function changeMessage2() {
+    document.getElementById("q2").innerHTML = "Love is in the air!";
+}
+function changeMessage3() {
+    document.getElementById("q3").innerHTML = "Ha, ha ,ha. LOL!";
+}
+function changeMessage4() {
+    document.getElementById("q4").innerHTML = "You nailed it!!!";
+}
