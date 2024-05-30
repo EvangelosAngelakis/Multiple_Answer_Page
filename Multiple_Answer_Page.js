e2012d1586988df8e1e5dd6fdbcaa6703bc29a07
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-05-30 15:44:59 +0100

    Added background Image & style color on appearing text

--- Multiple_Answer_Page.js
+++ Multiple_Answer_Page.js
@@ -1,12 +1,16 @@
 function changeMessage1() {
     document.getElementById("q1").innerHTML = "Great Answer";
+    document.getElementById("q1").style.color = "rgb(166, 233, 227)";
 }
 function changeMessage2() {
     document.getElementById("q2").innerHTML = "Love is in the air!";
+    document.getElementById("q2").style.color = "rgb(166, 233, 227)";
 }
 function changeMessage3() {
     document.getElementById("q3").innerHTML = "Ha, ha ,ha. LOL!";
+    document.getElementById("q3").style.color = "rgb(166, 233, 227)";
 }
 function changeMessage4() {
     document.getElementById("q4").innerHTML = "You nailed it!!!";
+    document.getElementById("q4").style.color = "rgb(166, 233, 227)";
 }
